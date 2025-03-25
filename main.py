4a771f7a4018935b354fe430aaa509287f5256ca
jacmnova <josealfre14@gmail.com>
2025-03-25 13:27:40 -0400

    ajsute

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, jsonify
 import requests
-from flask_cors import CORS  # Importa Flask-CORS
-
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
 url_base = 'http://186.167.69.10:50080/proteoerp/api'
 
@@ -12,16 +11,16 @@ url_base = 'http://186.167.69.10:50080/proteoerp/api'
 def login():
     user = request.form.get('user')
     password = request.form.get('password')
-    
+
     if not user or not password:
         return jsonify({'error': 'Missing user or password'}), 400
-    
+
     url = f'{url_base}/login/login'
     data = {'user': user, 'password': password}
-    
+
     try:
         response = requests.post(url, data=data)
-        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+        response.raise_for_status()
         return response.json(), response.status_code
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}), 500
@@ -31,8 +30,11 @@ def search_product():
     token = request.headers.get('Authorization')
     data = request.get_json()
 
+    if not token:
+        return jsonify({'error': 'Authorization header is missing'}), 401
+
     url = f'{url_base}/sinvalub/post/buscar'
-    headers = {'Authorization': token} if token else {}
+    headers = {'Authorization': token}
 
     try:
         response = requests.post(url, json=data, headers=headers)
@@ -46,8 +48,11 @@ def save_new_loc():
     token = request.headers.get('Authorization')
     data = request.get_json()
 
+    if not token:
+        return jsonify({'error': 'Authorization header is missing'}), 401
+
     url = f'{url_base}/sinvalub/post/actualizar'
-    headers = {'Authorization': token} if token else {}
+    headers = {'Authorization': token}
 
     try:
         response = requests.post(url, json=data, headers=headers)
@@ -57,4 +62,4 @@ def save_new_loc():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000) # Adjust host and port if needed
+    app.run(debug=True, host='0.0.0.0', port=5000)
